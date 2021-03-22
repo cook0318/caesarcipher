3d34e5f85dda0b2fda04a8b465739b55eac25656
Billy Cook <cook0318@algonquinlive.com>
2021-03-22 13:21:41 -0400

    added code to decrypt messages

--- caesar.py
+++ caesar.py
@@ -1,15 +1,22 @@
 # dictionary where "A":0, "B":1, etc
 from alphabet_dictionary import alphabet_dictionary
+# python built in library to check if word exists
+import enchant
 
 # making a list of the keys and values for easy access in functions below
 letters = list(alphabet_dictionary.keys())
 numbers = list(alphabet_dictionary.values())
+
+# dictionary used to check english words
+english_dictionary = enchant.Dict("en_US")
+
 
 def encrypt(message, key):
     try:
         key = int(key)
     except:
         print("The key provided is not a whole integer. Key: '" + key + "'")
+        return
     encrypted_characters = []
     characters = list(message)
     for char in characters:
@@ -25,7 +32,7 @@ def encrypt(message, key):
             char = char.upper()
             num = alphabet_dictionary[char]
             shifted_num = num + key
-            shifted_char_position = numbers.index(shifted_num)
+            shifted_char_position = get_shifted_char_position(shifted_num) 
             shifted_char = letters[shifted_char_position]
             encrypted_characters.append(shifted_char)
 
@@ -33,9 +40,44 @@ def encrypt(message, key):
     return encrypted_message
 
 
+def get_shifted_char_position(index):
+    if index > 25:
+        index = index % 26
+    elif index < 0:
+        while index < 0:
+            index = index + 26
+
+    return numbers.index(index)
 
 
+def decrypt(message):
+    # first split the message by spaces.
+    # if each word has a different cipher value, then this is necessary.
+    words = message.split(' ')
+    list_of_results = []
+    for word in words:
+        results = check_all_shifts(word)
+        list_of_results.append(results)
+    return list_of_results
 
+
+def check_all_shifts(encrypted_word):
+    results = []
+    for i in range(0,25):
+        # since the process of decrypting is the same as encrypting, we can use the same function.
+        decrypted_word = encrypt(encrypted_word, i)
+        if english_dictionary.check(decrypted_word) == True:
+            results.append(decrypted_word)
+    return results
+
+
+def print_results(encrypted_message, list_of_results):
+    # list_of_results is a list of lists. First list is possibilities for the first word, etc
+    words = encrypted_message.split(' ')
+    i = 0
+    for word in words:
+        print("Potential Decryptions for '{0}': {1}".format(word, ', '.join(list_of_results[i])))
+        i+=1
 
 
 def caeser_cipher():
@@ -50,15 +92,18 @@ def caeser_cipher():
             print("Only whole integers are accepted.")
             key = input(">")
             encrypted_message = encrypt(message,key)
-            print("Your encrypted message is: ")
-            print(encrypted_message)
-            prompt_user = False
+            if(encrypted_message is not None):
+                print("Your encrypted message is: ")
+                print(encrypted_message)
+                prompt_user = False
         elif(choice == "D" or choice == "d"):
-            print("decyrpting...")
-            prompt_user = False
+            print("Please enter the encrypted text.")
+            print("Only letters and spaces may be entered.")
+            message = input(">")
+            results = decrypt(message)
+            print_results(message, results)
         else:
             print("Invalid choice. Please enter E to encrypt or D to decrypt.")
             prompt_user = True
-
 
 caeser_cipher()
